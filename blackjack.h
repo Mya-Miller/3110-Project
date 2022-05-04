a2a6c0dffe0b33eec655ab247121c18e98e1e622
Emmie <36717379+Emmie8@users.noreply.github.com>
2022-05-04 18:03:45 -0500

    header file

---
+++ blackjack.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+
+
+using namespace std;
+class CardDeck{
+	private:
+	int cards[13] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+	vector<int> deck = vector<int>(52);
+
+	public:
+	void createDeck();
+	void shuffleDeck();
+	int passOut();
+	void printDeck();
+};
