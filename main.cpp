d56f866a2aaa59445aa3116200e556adb822515f
Mya-Miller <101530032+Mya-Miller@users.noreply.github.com>
2022-05-07 14:56:53 -0500

    added function where player can see their hand

--- main.cpp
+++ main.cpp
@@ -1,24 +1,39 @@
 #include <iostream>
+#include <vector>
+#include <iterator>
+#include <list>
+
 #include "CardDeck.h"
+#include "HardHand.h"
+#include "BettingSystem.h"
 
 using namespace std;
 
 int main(){
 	CardDeck deck; // creates object of cardDeck class
-  int winning_number; //holds the number the player want to get to
+  HardHand h_hand;
+  bettingSystem bet_sys;
+
+  //linked list that holds the players cards and dealers cards
+  list<int> players_hand;
+  list<int> dealers_hand;
+  
+  int bust_value; //holds the number the player want to get to
   int bet; //holds the amount the user wants to bet
   bool end_game = false, bust = false;
   int choice;
 
   cout << "Enter in the number you would like to reach? ";
-  cin >> winning_number;
+  cin >> bust_value;
+  h_hand.setBustValue(bust_value);
 
   cout << "How much money would you like to bet? $";
   cin >> bet;
+  bet_sys.setBetAmount(bet);
 
   deck.createDeck(); //creates deck
 	deck.shuffleDeck(); //shuffles deck
-
+  
   cout << endl;
   
   do{
@@ -40,17 +55,24 @@ int main(){
         
         //prints card value the user recived
         deck.displayCard(dealt_card);
+
+        //add card to players hand
+        players_hand.push_back(dealt_card);
         
-        //value needs to be sent to where ever the users card is being held
         //check if their is a bust
         //if bust player loses and game ends
-        //check if players hand == winning_number if so player wins game ends
+        //check if players hand == winning_number if so player wins and see if they want to 
         break;
       case 2:
         cout << "Turn skipped...\n";
         break;
       case 3:
-        cout << "Display players hand here...\n";
+        int x;
+        for (auto it = players_hand.begin(); it != players_hand.end(); ++it){
+          x = *it;
+          deck.displayHand(x);
+        }
+        cout << endl;
         break;
       case 4:
         cout << "Game terminating...\n";
@@ -59,7 +81,7 @@ int main(){
     }
 
     cout << endl;
-  }while(end_game != true && bust != true);
+  }while(end_game != true && bust != true); 
 
 	return 0;
 }
