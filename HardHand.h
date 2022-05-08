e25be65d04e965bffcab8d95430767a3e36daa9c
nazib1234 <44441569+nazib1234@users.noreply.github.com>
2022-05-07 21:09:42 -0500

    Update HardHand.h

--- HardHand.h
+++ HardHand.h
@@ -1,13 +1,22 @@
 #pragma once
+#include <vector>
+
+using namespace std;
 
 class HardHand {
 private:
-	int firstHalf; //Total Value first half
-	int secondHalf; //Total Value second half
+	vector<int> firstHalf; //Total Value first half
+	vector<int> secondHalf; //Total Value second half
   int bustValue; 
 public:
   void setBustValue(int);
   int getBustValue();
+  void setFirstHalf(int);
+  void setSecondHalf(int);
+  vector<int> getFirstHalf();
+  vector<int> getSecondHalf();
+  int FirstHalf(); //User will either hit or stand with their first pile
+  int SecondHalf(); //User will either hit or stand with their second pile
 	bool isBusted(); //Check if both variables above bust or not
 	bool isBustValue(); // if either the variables add up to 21
 	void showBothValues(); //Inform User total values of both
